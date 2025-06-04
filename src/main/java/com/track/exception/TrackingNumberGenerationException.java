6ff099723d194c5914c6fc916f12eb929d5697af
SaiKiran <saikiranreddyt@gmail.com>
2025-06-04 20:08:07 +0530

    fix tracker to generator unique,non sequence ids and refactor readme,exceptionHandler.

---
+++ src/main/java/com/track/exception/TrackingNumberGenerationException.java
@@ -0,0 +1,7 @@
+package com.track.exception;
+
+public class TrackingNumberGenerationException extends RuntimeException {
+    public TrackingNumberGenerationException(String message) {
+        super(message);
+    }
+} 
